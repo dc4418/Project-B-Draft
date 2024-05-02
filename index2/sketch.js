6d94833bc293f1f008e937fd5804ac2e7ec4fb86
dc4418 <dc4418@nyu.edu>
2024-05-02 11:04:06 +0800

    update May 2

---
+++ index2/sketch.js
@@ -0,0 +1,117 @@
+let message = "Most Populous Black Nation On Earth";
+let hiddenMessage = "_ _";
+
+let NUM_OF_PARTICLES = 40; // Decide the initial number of particles.
+let particles = [];
+
+function setup() {
+  createCanvas(600, 600);
+  textAlign(CENTER, CENTER);
+  textSize(30);
+
+  // Generate particles
+  for (let i = 0; i < NUM_OF_PARTICLES; i++) {
+    particles[i] = new Particle(random(width), random(height), random(30, 40));
+  }
+}
+
+function draw() {
+  background(220);
+
+  // Draw sand particles
+  drawSandParticles();
+
+  // Update and display particles
+  for (let i = 0; i < particles.length; i++) {
+    let p = particles[i];
+    p.update();
+    p.display();
+  }
+
+  // Draw messages
+  fill(0);
+  text(message, width/2, height/2 - 50);
+  text(hiddenMessage, width/2, height/2 + 50);
+}
+
+function keyPressed() {
+  if (key === 'n' || key === 'e') {
+    revealLetter(key);
+  }
+}
+
+function revealLetter(letter) {
+  if (letter === 'n') {
+    hiddenMessage = letter.toUpperCase() + hiddenMessage.charAt(1);
+  } else if (letter === 'e') {
+    hiddenMessage = hiddenMessage.charAt(0) + letter.toUpperCase() + hiddenMessage.charAt(3);
+  }
+}
+
+// Function to draw sand particles in the background
+function drawSandParticles() {
+  fill("grey");
+  for (let i = 0; i < 100; i++) {
+    let x = random(width);
+    let y = random(height);
+    ellipse(x, y, random(4, 8), random(4, 8)); // Random size sand particles
+  }
+}
+
+class Particle {
+  // constructor function
+  constructor(startX, startY) {
+    // properties: particle's characteristics
+    this.x = startX;
+    this.y = startY;
+    this.dia = 30;
+    this.vx = random(-1, 1); // velocity x
+    this.vy = random(-1, 1); // velocity y
+    this.ax = 0; // x direction acceleration
+    this.ay = 0; // y direction acceleration
+    this.maxSpeed = 3; // max speed
+  }
+
+  // methods (functions): particle's behaviors
+  update() {
+    // movement
+    this.ax += random(-0.1, 0.1);
+    this.ay += random(-0.1, 0.1);
+
+    // Acceleration Constrain
+    if (this.ax < -0.1) this.ax = -0.1;
+    if (this.ax > 0.1) this.ax = 0.1;
+    if (this.ay < -0.1) this.ay = -0.1;
+    if (this.ay > 0.1) this.ay = 0.1;
+
+    // Update Velocity
+    this.vx += this.ax;
+    this.vy += this.ay;
+
+    // Velocity Constrain
+    if (this.vx < -this.maxSpeed) this.vx = -this.maxSpeed;
+    if (this.vx > this.maxSpeed) this.vx = this.maxSpeed;
+    if (this.vy < -this.maxSpeed) this.vy = -this.maxSpeed;
+    if (this.vy > this.maxSpeed) this.vy = this.maxSpeed;
+
+    // Update Position
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Respawn Points
+    if (this.x < -this.dia) this.x = width + this.dia;
+    if (this.y < -this.dia) this.y = height + this.dia;
+    if (this.x > width + this.dia) this.x = -this.dia;
+    if (this.y > height + this.dia) this.y = -this.dia;
+  }
+
+  display() {
+    // particle's appearance
+    push();
+    translate(this.x, this.y);
+    fill("red");
+    noStroke();
+    circle(0, 0, random(20,30));
+    pop();
+  }
+}
---
+++ index3/sketch.js
@@ -0,0 +1,98 @@
+var capture;
+var tracker;
+var w = 640,
+    h = 480;
+var mouthOpenThreshold = 100; // Adjust this threshold as needed
+var cigaretteLength = 150;
+var cigaretteWidth = 15;
+
+function setup() {
+    capture = createCapture({
+        audio: false,
+        video: {
+            width: w,
+            height: h
+        }
+    }, function() {
+        console.log('capture ready.')
+    });
+    capture.elt.setAttribute('playsinline', '');
+    createCanvas(w, h);
+    capture.size(w, h);
+    capture.hide();
+
+    colorMode(HSB);
+
+    tracker = new clm.tracker();
+    tracker.init();
+    tracker.start(capture.elt);
+}
+
+function draw() {
+    image(capture, 0, 0, w, h);
+    var positions = tracker.getCurrentPosition();
+
+    noFill();
+    stroke(255);
+    beginShape();
+    for (var i = 0; i < positions.length; i++) {
+        vertex(positions[i][0], positions[i][1]);
+    }
+    endShape();
+
+    noStroke();
+    for (var i = 0; i < positions.length; i++) {
+        fill(map(i, 0, positions.length, 0, 360), 50, 100);
+        ellipse(positions[i][0], positions[i][1], 4, 4);
+        text(i, positions[i][0], positions[i][1]);
+      textSize(0)
+    }
+
+    if (positions.length > 0) {
+        var mouthTopY = positions[57][1]; // Changed index to 57 for top lip
+        var mouthBottomY = positions[60][1]; // Changed index to 60 for bottom lip
+        var mouthOpen = mouthBottomY - mouthTopY; // Calculate the vertical distance
+
+        // Calculate the difference between current mouth open and threshold
+        var difference = mouthOpenThreshold - mouthOpen;
+        console.log('Difference to Threshold:', difference);
+
+        // Check if mouth is open based on the difference in y-coordinates
+        if (mouthOpen <= mouthOpenThreshold) {
+            // Draw black background if mouth is open
+            background(0);
+            textSize(32);
+            textAlign(CENTER, CENTER);
+            text('smoking is bad____', width/2, height/2);
+          text('smoking is bad____', width/2, height/3);
+          text('smoking is bad____', width/2, height/4);
+          text('smoking is bad____', width/2, height/5);
+          text('smoking is bad____', width/2, height/6);
+          text('smoking is bad____', width/2, height/7);
+          text('smoking is bad____', width/2, height/8);
+          text('smoking is bad____', width/2, height/9);
+          text('smoking is bad____', width/2, height/10);
+          text('smoking is bad____', width/2, height/11);
+          text('smoking is bad____', width/2, height/12);
+          text('smoking is bad____', width/2, height/13);
+          text('COUNT your days >_<', width/2, height/1.2);
+          
+        } else {
+            // Draw anything else you like when mouth is closed
+            // For example, you can draw the cigarette-like shape here
+            drawCigarette(positions[62][0], positions[62][1]);
+        }
+    }
+}
+
+function drawCigarette(x, y) {
+    // Draw the white part of the cigarette
+    fill(255); // White color for the cigarette tip
+    var whiteLength = cigaretteLength * 0.75; // 3/4 of the cigarette length
+    rectMode(CENTER);
+    rect(x/2, y + whiteLength * 0.5, cigaretteWidth, whiteLength*2, 5);
+
+    // Draw the yellowish part of the cigarette
+    fill(40, 100, 100); // Yellowish color for the cigarette filler
+    rect(x/2, y - whiteLength * 0.5, cigaretteWidth, whiteLength, 5);
+}
